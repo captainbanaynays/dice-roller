25851371a32ab8a8c716fee935195ccf503fd2ee
Ethan Meltzer <captainbanaynays@gmail.com>
2023-10-07 14:48:04 -0400

    Added line history, advantage, and disadvantage!

--- src/main.rs
+++ src/main.rs
@@ -1,75 +1,89 @@
-use std::io::{self, Write};
 use std::cmp::{min, max};
 use rand::Rng;
 use regex::Regex;
+use rustyline::{
+    error::ReadlineError,
+    DefaultEditor,
+};
 
 fn main() {
-    let dice_re_1 = Regex::new(r"(\d*)[dD](\d+)\s*([\+-]?)\s*(\d*)\s*-*([daDA]*
-        )").unwrap();
+    let dice_re_1 = Regex::new(r"(\d*)[dD](\d+)\s*([\+-]?)\s*(\d*)\s*-*([daDA]*)").unwrap();
     let dice_re_d20 = Regex::new(r"([+-]?)\s*(\d*)\s*-*([daDA]?)").unwrap();
-    let mut input_buffer = String::new();
+    let mut rl = DefaultEditor::new().unwrap();
         
     loop {
-        print!("Roll: ");
-        io::stdout().flush().unwrap();
+        let readline = rl.readline("⚀ ⚁ ⚂ ⚃ ⚄ ⚅ ");
 
-        if io::stdin().read_line(&mut input_buffer).unwrap() == 0 {
-            break;
-        }
+        match readline{
+            Ok(line) => {
+                rl.add_history_entry(line.as_str()).unwrap();
+                let (nd, ns, pm, w, ad): (u8, u8, &str, u8, &str);
+                if line == "\n" || line == "\r\n" {
+                    (nd, ns, pm, w, ad) = (1u8, 20u8, "", 0u8, "");
+                } else if let Some(val) = dice_re_1.captures(&line) {
+                    let (_, [nds, nss, pms, ws, ads]) = val.extract();
 
-        let (nd, ns, pm, w, ad): (u8, u8, &str, u8, &str);
-        if input_buffer == "\n" || input_buffer == "\r\n" {
-            (nd, ns, pm, w, ad) = (1u8, 20u8, "", 0u8, "");
-        } else if let Some(val) = dice_re_1.captures(&input_buffer) {
-            let (_, [nds, nss, pms, ws, ads]) = val.extract();
+                    (nd, ns, pm, w, ad) = (nds.parse::<u8>().unwrap_or(1u8),
+                        nss.parse::<u8>().unwrap_or(20u8),
+                        pms,
+                        ws.parse::<u8>().unwrap_or(0u8),
+                        ads);
+                } else if let Some(val) = dice_re_d20.captures(&line) {
+                    let (_, [pms, ws, ads]) = val.extract();
 
-            (nd, ns, pm, w, ad) = (nds.parse::<u8>().unwrap_or(1u8),
-                nss.parse::<u8>().unwrap_or(20u8),
-                pms,
-                ws.parse::<u8>().unwrap_or(0u8),
-                ads);
-        } else if let Some(val) = dice_re_d20.captures(&input_buffer) {
-            let (_, [pms, ws, ads]) = val.extract();
+                    (nd, ns, pm, w, ad) = (1, 20, pms, ws.parse::<u8>()
+                        .unwrap_or(0u8), ads);
 
-            (nd, ns, pm, w, ad) = (1, 20, pms, ws.parse::<u8>().unwrap_or(0u8),
-            ads);
+                } else {
+                    println!();
+                    continue;
+                }
 
-        } else {
-            println!();
-            continue;
-        }
-
-        // Advantage and disadvantage handling
-        match ad.to_lowercase().as_str() {
-            "a" => {
-                print!("Roll 1: ");
-                let roll_1 = roll_dice(nd, ns, pm, w);
-                println!();
-                print!("Roll 2: ");
-                let roll_2 = roll_dice(nd, ns, pm, w);
-                println!();
-                let result = max(roll_1, roll_2);
-                println!("Result: {result}");
-                println!();
+                // Advantage and disadvantage handling
+                match ad.to_lowercase().as_str() {
+                    "a" => {
+                        print!("Roll 1: ");
+                        let roll_1 = roll_dice(nd, ns, pm, w);
+                        println!();
+                        print!("Roll 2: ");
+                        let roll_2 = roll_dice(nd, ns, pm, w);
+                        println!();
+                        let result = max(roll_1, roll_2);
+                        println!("Result: {result}");
+                        println!();
+                    },
+                    "d" => {
+                        print!("Roll 1: ");
+                        let roll_1 = roll_dice(nd, ns, pm, w);
+                        println!();
+                        print!("Roll 2: ");
+                        let roll_2 = roll_dice(nd, ns, pm, w);
+                        println!();
+                        let result = min(roll_1, roll_2);
+                        println!("Result: {result}");
+                        println!();
+                    },
+                    _ => {
+                        print!("Roll: ");
+                        let result = roll_dice(nd, ns, pm, w);
+                        println!();
+                        println!("Result: {result}");
+                        println!();
+                    },
+                }
             },
-            "d" => {
-                print!("Roll 1: ");
-                let roll_1 = roll_dice(nd, ns, pm, w);
-                println!();
-                print!("Roll 2: ");
-                let roll_2 = roll_dice(nd, ns, pm, w);
-                println!();
-                let result = min(roll_1, roll_2);
-                println!("Result: {result}");
-                println!();
+            Err(ReadlineError::Interrupted) => {
+                println!("Interrupt signal received.");
+                break
             },
-            _ => {
-                print!("Roll: ");
-                let result = roll_dice(nd, ns, pm, w);
-                println!();
-                println!("Result: {result}");
-                println!();
-            },
+            Err(ReadlineError::Eof) => {
+                println!("Roll again with me sometime!");
+                break
+            }
+            Err(err) => {
+                println!("Error: {err:?}");
+                break
+            }
         }
     }
 }
