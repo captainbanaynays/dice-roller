35597ebe6e2286568d04c57470ba8cdaff6c1aba
Ethan Meltzer <captainbanaynays@gmail.com>
2023-10-07 02:06:18 -0400

    started advantage/disadvantage

--- src/main.rs
+++ src/main.rs
@@ -3,14 +3,13 @@ use rand::Rng;
 use regex::Regex;
 
 fn main() {
-    let dice_re_1 = Regex::new(r"(\d*)d(\d+)\s*([\+-]?)\s*(\d*)").unwrap();
-    let dice_re_d20 = Regex::new(r"([+-]?)\s*(\d+)").unwrap();
+    let dice_re_1 = Regex::new(r"(\d*)[dD](\d+)\s*([\+-]?)\s*(\d*)\s*-*([daDA]*
+        )").unwrap();
+    let dice_re_d20 = Regex::new(r"([+-]?)\s*(\d*)\s*-*([daDA]?)").unwrap();
     let mut input_buffer = String::new();
-    let mut rng = rand::thread_rng();
-    
 
     loop {
-        print!("r ");
+        print!("Roll: ");
         io::stdout().flush().unwrap();
 
         if io::stdin().read_line(&mut input_buffer).unwrap() == 0 {
@@ -19,25 +18,32 @@ fn main() {
 
         let mut result = 0;
 
-        let (nd, ns, pm, w): (u8, u8, &str, u8);
-        if input_buffer == "\n" {
-            (nd, ns, pm, w) = (1u8, 20u8, "", 0u8);
+        let (nd, ns, pm, w, ad): (u8, u8, &str, u8, &str);
+        if input_buffer == "\n" || input_buffer == "\r\n" {
+            (nd, ns, pm, w, ad) = (1u8, 20u8, "", 0u8, "");
         } else if let Some(val) = dice_re_1.captures(&input_buffer) {
-            let (_, [nds, nss, pms, ws]) = val.extract();
+            let (_, [nds, nss, pms, ws, ads]) = val.extract();
 
-            (nd, ns, pm, w) = (nds.parse::<u8>().unwrap_or(1u8),
+            (nd, ns, pm, w, ad) = (nds.parse::<u8>().unwrap_or(1u8),
                 nss.parse::<u8>().unwrap_or(20u8),
                 pms,
-                ws.parse::<u8>().unwrap_or(0u8));
+                ws.parse::<u8>().unwrap_or(0u8),
+                ads);
         } else if let Some(val) = dice_re_d20.captures(&input_buffer) {
-            let (_, [pms, ws]) = val.extract();
+            let (_, [pms, ws, ads]) = val.extract();
 
-            (nd, ns, pm, w) = (1, 20, pms, ws.parse::<u8>().unwrap());
+            (nd, ns, pm, w, ad) = (1, 20, pms, ws.parse::<u8>().unwrap_or(0u8),
+            ads);
+
         } else {
             println!();
             continue;
         }
 
+        // Advantage and disadvantage handling
+        if ad.to_lowercase() == "a" {
+            print!("Roll 1: ");
+        }
         print!("Rolls: ");
         for _ in 0..nd {
             let roll = rng.gen_range(1..=ns);
@@ -56,4 +62,15 @@ fn main() {
         println!("Result: {result}");
         println!();
     }
+}
+
+fn roll_dice(nd:u8, ns:u8) -> u8 {
+    let mut rng = rand::thread_rng();
+    let mut result = 0;
+    for _ in 0..nd {
+        let roll = rng.gen_range(1..=ns);
+        result += roll;
+        print!("{roll} ");
+    }
+    result
 }
